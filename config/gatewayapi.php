e9618a6b03acafce23ad4b0a41fb36b125140287
Ruslan Popelyshyn <45109375+Loafer19@users.noreply.github.com>
2021-07-08 17:56:34 +0300

    added config, upd composer

---
+++ config/gatewayapi.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Credentials
+    |--------------------------------------------------------------------------
+    |
+    | If you're using API credentials, change these settings. Get your
+    | credentials from https://gatewayapi.com/app 'API/API Keys'.
+    |
+    */
+
+    'api_key'    => env('GATEWAY_KEY', ''),
+    'api_secret' => env('GATEWAY_SECRET', ''),
+];
